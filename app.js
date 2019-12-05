2dca53524bcf26727020183f14dceafeddaba93d
24zawa <53282148+24zawa@users.noreply.github.com>
2019-12-05 14:34:47 +0900

    indexを変更しました

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 7000;
+
+//publicを使えるようにする
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/' , function(req, res){
     res.sendFile(__dirname+'/index.html');
